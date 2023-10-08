9651810b0f4ea0a05e3079fb66308a001ff70d15
someuser32 <102308925+someuser32@users.noreply.github.com>
2023-10-08 21:38:40 +0600

    some procedure programming

--- api/login.php
+++ api/login.php
@@ -1,35 +1,52 @@
 <?php
+	require("config.php");
+
+	function login($username, $password) {
+		$client = new MongoDB\Client(MONGODB_CONNECTION);
+		$users = $client->firstphp->users;
+
+		$user = $users->findOne(["username" => $username]);
+
+		if (is_null($user)) {
+			return null;
+		};
+
+		$password = hash("sha256", $password."+".$user["password_salt"]);
+
+		if ($password != $user["password"]) {
+			return false;
+		};
+
+		return $user;
+	};
+
+	function do_login($params) {
+		$default_params = array("username", "password");
+
+		if (count(array_intersect_key(array_flip($default_params), $_POST)) != count($default_params)) {
+			echo "bad request!";
+			return null;
+		};
+
+		$user = login($_POST["username"], $_POST["password"]);
+
+		if (is_null($user)) {
+			echo "username does not exists";
+			return false;
+		} else if (is_bool($user) && $user == false) {
+			echo "incorrect password!";
+			return false;
+		};
+
+		echo "logged in!";
+		return true;
+	};
+
+
 	if (array_key_exists("is_registration", $_POST) && $_POST["is_registration"] == 1) {
 		echo "not implemented yet!";
 		return;
 	};
 
-	$login_params = array("username", "password");
-
-	if (count(array_intersect_key(array_flip($login_params), $_POST)) != count($login_params)) {
-		echo "bad request!";
-		return;
-	};
-
-	require("config.php");
-
-	$client = new MongoDB\Client(MONGODB_CONNECTION);
-
-	$users = $client->firstphp->users;
-
-	$user = $users->findOne(["username" => $_POST["username"]]);
-
-	if (is_null($user)) {
-		echo "username not exists";
-		return;
-	};
-
-	$password = hash("sha256", $_POST["password"]."+".$user["password_salt"]);
-
-	if ($password != $user["password"]) {
-		echo "password is incorrect!";
-		return;
-	};
-
-	echo "logged in!";
+	do_login($_POST);
 ?>
