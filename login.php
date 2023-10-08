0cb282f1eecccd782b714816f24d7c79f78290c2
someuser32 <drodochess@gmail.com>
2023-10-08 20:23:54 +0600

    dummy login

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<link rel="stylesheet" href="css/styles.css" type="text/css">
-		<link rel="stylesheet" href="css/index/index.css" type="text/css">
+		<link rel="stylesheet" href="css/login/index.css" type="text/css">
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=AR+One+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
@@ -16,6 +16,25 @@
 		<div id="Body">
 			<div class="Container">
 				<div class="Row">
+					<table class="LoginContainer">
+						<tbody>
+							<tr class="Username">
+								<td>Username:</td>
+								<td><input name="username" type="text"></td>
+							</tr>
+							<tr class="Password">
+								<td>Password:</td>
+								<td><input name="password" type="password"></td>
+							</tr>
+							<tr class="Submit">
+								<td></td>
+								<td>
+									<label><input name="save" type="checkbox">Save password?</label>
+									<button class="right" onclick="alert('Hello, world!')">Login</button>
+								</td>
+							</tr>
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
