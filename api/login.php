1f8a853a3df82f111ef18c6122d41e13b3564a46
someuser32 <102308925+someuser32@users.noreply.github.com>
2023-10-08 21:28:57 +0600

    some work with mongodb
    i already worked with mongodb in python, so decided to also use it here, why not, they give us free 512MB storage

--- api/login.php
+++ api/login.php
@@ -1,5 +1,35 @@
 <?php
+	if (array_key_exists("is_registration", $_POST) && $_POST["is_registration"] == 1) {
+		echo "not implemented yet!";
+		return;
+	};
+
+	$login_params = array("username", "password");
+
+	if (count(array_intersect_key(array_flip($login_params), $_POST)) != count($login_params)) {
+		echo "bad request!";
+		return;
+	};
+
 	require("config.php");
 
-	echo MONGODB_CONNECTION;
+	$client = new MongoDB\Client(MONGODB_CONNECTION);
+
+	$users = $client->firstphp->users;
+
+	$user = $users->findOne(["username" => $_POST["username"]]);
+
+	if (is_null($user)) {
+		echo "username not exists";
+		return;
+	};
+
+	$password = hash("sha256", $_POST["password"]."+".$user["password_salt"]);
+
+	if ($password != $user["password"]) {
+		echo "password is incorrect!";
+		return;
+	};
+
+	echo "logged in!";
 ?>
